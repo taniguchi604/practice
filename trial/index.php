20fd7ba4f924989a7322758b65bd8ca854034585
谷口 裕明 <taniguchi604@yahoo.co.jp>
2018-12-13 16:18:52 +0900

    更新

---
+++ trial/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+	<meta charset="utf-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<link rel="stylesheet" type="text/css" href="style.css" media="screen">
+
+	<title>Bridge</title>
+
+</head>
+
+<body>
+
+	<!-- ヘッダーここから -->
+	<header>
+
+		<h1>Lending Platform</h1>
+
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		    	<span class="navbar-toggler-icon"></span>
+		 	</button>
+		 	<div class="collapse navbar-collapse" id="navbarNav">
+		    	<ul class="navbar-nav">
+		      		<li class="nav-item active">
+		        		<a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+		      		</li>
+		      		<li class="nav-item">
+		        		<a class="nav-link" href="#">借りたい</a>
+		      		</li>
+		      		<li class="nav-item">
+		        		<a class="nav-link" href="#">貸したい</a>
+		      		</li>
+		      		<li class="nav-item">
+		        		<a class="nav-link" href="#">About Bridge</a>
+		      		</li>
+		      		<li class="nav-item">
+		        		<a class="nav-link" href="#">News</a>
+		      		</li>
+		      		<li class="nav-item">
+		        		<a class="nav-link" href="#">Sign in</a>
+		      		</li>
+		    	</ul>
+		  	</div>
+		</nav>
+
+	</header>
+	<!-- ヘッダーここまで -->
+
+	<!-- ラッパーここから -->
+	<div id="wrapper">
+
+		<!-- メイン画像ここから -->
+		<div id="mainimage"></div>
+		<!-- メイン画像ここまで -->
+
+		<div id="description"></div>
+
+		<div id="applications"></div>
+
+	</div>
+	<!-- ラッパーここまで -->
+
+	<!-- フッターここから -->
+	<footer></footer>
+	<!-- フッターここまで -->
+
+	<!-- スクリプトここから -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<!-- スクリプトここまで -->
+
+</body>
+
+</html>
